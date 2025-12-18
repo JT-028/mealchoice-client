46eee5636e17ce0dce2fc5bdf1f685dbb9c5b8b4
Mark Bennett N. Pineda <markbennettpineda@gmail.com>
2025-12-18 15:59:35 +0800

    feat: scaffold basic React application with health check and meal management, integrating Tailwind CSS and updating build configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { fetchHealth } from './api'
 import { fetchMeals, createMeal, type Meal } from './api/meals'
 
@@ -41,14 +38,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Mealwise + MERN Stack</h1>
       
       <div className="card">
